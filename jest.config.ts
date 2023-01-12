22f4511778a7598d3228e39718f985c6fdd23e12
SummerJ <summer@codebyjordan.com>
2023-01-11 19:08:48 -0700

    test: add tsconfig.test.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,15 @@
 import type { Config } from 'jest'
 
 const config: Config = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json'
+      },
+    ],
+  },
 }
 
 export default config
